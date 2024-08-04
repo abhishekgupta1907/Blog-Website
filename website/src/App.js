1b1543d1e59e2247b9be1a418d3e9e9418d8a513
Abhishek Gupta <abhishekgupta8967@gmail.com>
2024-08-04 14:15:55 +0530

    changing content

---
+++ website/src/App.js
@@ -0,0 +1,11 @@
+import "./App.css";
+import React from "react";
+function App() {
+    return (
+        <>
+            <h1 className="bg-green-100 text-5xl"> Hello</h1>
+        </>
+    );
+}
+
+export default App;
