4c7c4e8f5cba6503fea9775858d54239d3881503
Abhishek Gupta <abhishekgupta8967@gmail.com>
2024-08-04 20:22:43 +0530

    adding files

--- website/src/App.js
+++ website/src/App.js
@@ -1,9 +1,11 @@
 import "./App.css";
 import React from "react";
+import Home from "./Pages/Home";
 function App() {
     return (
         <>
             <h1 className="bg-green-100 text-5xl"> Hello</h1>
+            <Home />
         </>
     );
 }
